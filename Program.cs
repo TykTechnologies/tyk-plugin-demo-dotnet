cf050974ce32f438a29b669e8f8ad5ae666303de
Matias Insaurralde <matias@insaurral.de>
2016-10-21 02:14:15 -0300

    Push sample server.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+using System;
+using Grpc.Core;
 
 namespace ConsoleApplication
 {
-    public class Program
+  public class Program
+  {
+    // Make sure your Tyk instance is pointing to this port (tcp://127.0.0.1:5555):
+    const int Port = 5555;
+
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Server server = new Server{
+        Services = { Coprocess.Dispatcher.BindService(new DispatcherImpl()) },
+        Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+      };
+      server.Start();
+
+      Console.WriteLine("gRPC / CP server listening on " + Port);
+      Console.WriteLine("Press any key to stop the server...");
+      Console.ReadKey();
+
+      server.ShutdownAsync().Wait();
     }
+  }
 }
